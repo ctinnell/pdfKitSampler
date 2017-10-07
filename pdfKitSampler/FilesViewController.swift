7340cf23f6e295bb8f4db0fe995ac94e3afe63f0
Clay Tinnell <ctinnell@gaig.com>
2017-10-07 11:44:59 -0400

    pdfs loading

--- pdfKitSampler/FilesViewController.swift
+++ pdfKitSampler/FilesViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class FilesViewController: UITableViewController {
 
+    let books = ["Beyond Code",
+                 "Hacking with macOS",
+                 "Hacking with Swift",
+                 "Hacking with tvOS",
+                 "Objective-C for Swift Developers",
+                 "Practical iOS 10",
+                 "Practical iOS 11",
+                 "Pro Swift",
+                 "Server-Side Swift",
+                 "Swift Coding Challenges"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "Books"
         // Do any additional setup after loading the view.
     }
 
@@ -22,14 +33,27 @@ class FilesViewController: UITableViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = books[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let navController = splitViewController?.viewControllers[1] as? UINavigationController else { return }
+        guard let viewController = navController.viewControllers[0] as? ViewController else { return }
+        
+        viewController.load(books[indexPath.row])
+    }
 
 }
--- pdfKitSampler/ViewController.swift
+++ pdfKitSampler/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import PDFKit
 
 class ViewController: UIViewController {
 
+    let pdfView = PDFView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        pdfView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pdfView)
+        pdfView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        pdfView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        pdfView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        pdfView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func load(_ name: String) {
+        let filename = name.replacingOccurrences(of: " ", with: "-").lowercased()
+        guard let path = Bundle.main.url(forResource: filename, withExtension: "pdf") else { return }
+        
+        if let document = PDFDocument(url: path) {
+            pdfView.document = document
+            
+            pdfView.goToFirstPage(nil)
+            
+            if UIDevice.current.userInterfaceIdiom == .pad {
+                title = name
+            }
+        }
+    }
+    
 }
 
