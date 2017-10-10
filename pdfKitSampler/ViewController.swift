b4bcb30f89d598846c20c107fc205d4e21bf6d81
Clay Tinnell <ctinnell@gaig.com>
2017-10-09 21:08:45 -0400

    added text view option

--- pdfKitSampler/ViewController.swift
+++ pdfKitSampler/ViewController.swift
@@ -13,16 +13,36 @@ import SafariServices
 class ViewController: UIViewController {
 
     let pdfView = PDFView()
+    let textView = UITextView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let viewMode = UISegmentedControl(items: ["PDF","Text"])
+        viewMode.addTarget(self, action: #selector(changeViewMode), for: .valueChanged)
+        viewMode.selectedSegmentIndex = 0
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: viewMode)
+        navigationItem.rightBarButtonItem?.width = 150
+        
         pdfView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(pdfView)
         pdfView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         pdfView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         pdfView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
         pdfView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(textView)
+        textView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        textView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        textView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        textView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         
+        textView.isEditable = false
+        textView.isHidden = true
+        textView.textContainerInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+
         let previous = UIBarButtonItem(barButtonSystemItem: .rewind, target: pdfView, action: #selector(PDFView.goToPreviousPage(_:)))
         let next = UIBarButtonItem(barButtonSystemItem: .fastForward, target: pdfView, action: #selector(PDFView.goToNextPage(_:)))
         let search = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(promptForSearch))
@@ -46,7 +66,6 @@ class ViewController: UIViewController {
             pdfView.document = document
             
             pdfView.goToFirstPage(nil)
-            
             if UIDevice.current.userInterfaceIdiom == .pad {
                 title = name
             }
@@ -80,6 +99,46 @@ class ViewController: UIViewController {
         let vc = UIActivityViewController(activityItems: [selection], applicationActivities: nil)
         vc.popoverPresentationController?.barButtonItem = sender
         present(vc, animated: true)
+    }
+    
+    @objc func changeViewMode(segmentedControl: UISegmentedControl) {
+        
+        if segmentedControl.selectedSegmentIndex == 0 {
+            pdfView.isHidden = false
+            textView.isHidden = true
+        }
+        else {
+            pdfView.isHidden = true
+            textView.isHidden = false
+            loadText()
+        }
+    }
+    
+    func loadText() {
+        guard let pageCount = pdfView.document?.pageCount else { return }
+        
+        let documentContent = NSMutableAttributedString()
+        
+        for i in 1 ..< pageCount {
+            guard let page = pdfView.document?.page(at: i) else { continue }
+            guard let pageContent = page.attributedString else { continue }
+            
+            let spacer = NSAttributedString(string: "\n\n")
+            documentContent.append(spacer)
+            documentContent.append(pageContent)
+        }
+        
+        let pattern = "www.hackingwithswift.com [0-9]{1,2}"
+        let regex = try? NSRegularExpression(pattern: pattern)
+        let range = NSMakeRange(0, documentContent.string.utf16.count)
+        
+        if let matches = regex?.matches(in: documentContent.string, options: [], range: range) {
+            for match in matches.reversed() {
+                documentContent.replaceCharacters(in: match.range, with: "")
+            }
+        }
+        
+        textView.attributedText = documentContent
     }
 }
 
